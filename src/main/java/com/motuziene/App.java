ab94aa0aea77774a01394f63970587a435d52244
Ingrida <motuziene47@gmail.com>
2023-10-21 11:25:21 +0300

    Antras bandymas

--- src/main/java/com/motuziene/App.java
+++ src/main/java/com/motuziene/App.java
@@ -1,11 +1,17 @@
 package com.motuziene;
 
+import java.util.HashMap;
+import java.util.Scanner;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
+public class App {
+
+    public static final Scanner SCANNER = new Scanner(System.in);
+    public static final HashMap<String, String> VARTOTOJO_SAUGYKLA = new HashMap<String, String>();
+
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
